860fdbb5c22eaf2e2beb908a015d5a89f4f5e5e7
AndrewRSlavin <andrewryanslavin@gmail.com>
2024-11-13 22:17:46 -0500

    Update StudentDatabase.m

--- StudentDatabase.m
+++ StudentDatabase.m
@@ -5,28 +5,30 @@
 % Task: Main Script
 % Date: 11/13/2024
 
+% 
 classdef StudentDatabase
-    % properties
+    properties
         Students = [];
     end
     
-    % methods
+    methods
+    % Adds students
         function obj = addStudent(obj, student)
             obj.Students = [obj.Students; student];
         end
-        
+    % Finds students and sorts them by ID
         function student = findStudentByID(obj, id)
             student = obj.Students(strcmp({obj.Students.ID}, id));
         end
-        
+    % Find students by major
         function list = getStudentsByMajor(obj, major)
             list = obj.Students(strcmp({obj.Students.Major}, major));
         end
-        
+    % Saves the student to a file
         function saveToFile(obj, filename)
             save(filename, 'obj');
         end
-        
+    % Loads a saved student from the file
         function obj = loadFromFile(obj, filename)
             loadedData = load(filename);
             obj = loadedData.obj;
